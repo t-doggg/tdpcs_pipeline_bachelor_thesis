f9a5ac5d9cc181a6420b3815097542e758fdd4a9
t-doggg <129484171+t-doggg@users.noreply.github.com>
2024-11-30 08:09:58 +0100

    Update run_tdcs_long_single.sh

--- run_tdcs_long_single.sh
+++ run_tdcs_long_single.sh
@@ -196,124 +196,7 @@ else
 fi
 #-------------------------------------------------------------------------------------------------------------------------------------
 
-# Wenn der Ordner $OUTDIR/03-Bins bereits existiert, dann überspringe
-if [ -d "$OUTDIR/03-Bins" ]; then
-	echo "$(datetime.warning) ------------------------------------------------------------------------------------------------------------------------" >> "$G_LOG_FILE"
-	echo "$(datetime.skip) DeNovo Assembly wird uebersprungen" >> "$G_LOG_FILE"
-	echo "$(datetime.skip) DeNovo Assembly wird uebersprungen" 
 
-else
-	### DeNovo Assembly
-	# Definiere den Pfad zur Protokolldatei
-	FLYE_LOG="$OUTDIR/02-De_Novo_Assembly/de_novo_assembly.log"
-	# Erstelle das Verzeichnis, falls es nicht vorhanden ist
-	mkdir -p "$(dirname "$ALIGN_LOG")"
-	# Überprüfe, ob alle erforderlichen Variablen gesetzt sind
-	if [ -z "$OUTDIR" ] || [ -z "$THREADS" ] || [ -z "$REFHUM" ]; then
-		echo "$(datetime.error) Erforderliche Variablen fehlen."
-		exit 1
-	fi
-	
-	# Erstelle das Verzeichnis, falls es nicht vorhanden ist
-	mkdir -p "$(dirname "$FLYE_LOG")"
-	# Überprüfe, ob alle erforderlichen Variablen gesetzt sind
-	if [ -z "$OUTDIR" ] || [ -z "$THREADS" ]; then
-		echo "$(datetime.error) Erforderliche Variablen fehlen."
-		exit 1
-	fi
-
-	# Ausgabe des Startzeitpunkts in die Protokolldatei
-	echo "$(datetime.task) Starte De-Novo-Assembly" >> "$FLYE_LOG"
-	echo "$(datetime.task) Starte De-Novo-Assembly" >> "$G_LOG_FILE"
-
-	# Assembly mit Flye durchführen
-	mkdir -p "$OUTDIR/02-De_Novo_Assembly"
-	flye --meta --nano-hq "$OUTDIR/01-CleanedReads/align_to_ref_unaligned.fastq" -t "$THREADS" -o "$OUTDIR/02-De_Novo_Assembly/Flye"
-
-	# Ausgabe, dass der Vorgang abgeschlossen ist und Protokoll in die Datei schreiben
-	echo "$(datetime.done) De-Novo-Assembly abgeschlossen. Ausgabedaten in $OUTDIR/02-De_Novo_Assembly/Flye" | tee -a "$FLYE_LOG"
-	echo "$(datetime.done) De-Novo-Assembly abgeschlossen. Ausgabedaten in $OUTDIR/02-De_Novo_Assembly/Flye" >> "$G_LOG_FILE"
-	echo "$(datetime.done) ------------------------------------------------------------------------------------------------------------------------" >> "$G_LOG_FILE"
-	echo "$(datetime.done)  _____  _      __ __    ___      ___     ___   ____     ___ "
-	echo "$(datetime.done) |     || |    |  |  |  /  _]    |   \   /   \ |    \   /  _]"
-	echo "$(datetime.done) |   __|| |    |  |  | /  [_     |    \ |     ||  _  | /  [_ "
-	echo "$(datetime.done) |  |_  | |___ |  ~  ||    _]    |  D  ||  O  ||  |  ||    _]"
-	echo "$(datetime.done) |   _] |     ||___, ||   [_     |     ||     ||  |  ||   [_ "
-	echo "$(datetime.done) |  |   |     ||     ||     |    |     ||     ||  |  ||     |"
-	echo "$(datetime.done) |__|   |_____||____/ |_____|    |_____| \___/ |__|__||_____|"
-fi
-#-------------------------------------------------------------------------------------------------------------------------------------
-
-# Wenn der Ordner $OUTDIR/04-Classify bereits existiert, dann überspringe
-if [ -d "$OUTDIR/04-Classify" ]; then
-	echo "$(datetime.warning) ------------------------------------------------------------------------------------------------------------------------" >> "$G_LOG_FILE"
-	echo "$(datetime.skip) Binning mit Metawrap & Binning-Verfeinerung mit Metawrap wird uebersprungen" >> "$G_LOG_FILE"
-		echo "$(datetime.skip) Binning mit Metawrap & Binning-Verfeinerung mit Metawrap wird uebersprungen" 
-else
-		### Binning mit Metawrap
-	# Definiere den Pfad zur Protokolldatei
-	BINNING_LOG="$OUTDIR/03-Bins/binning.log"
-
-	# Erstelle das Verzeichnis, falls es nicht vorhanden ist
-	mkdir -p "$(dirname "$BINNING_LOG")"
-
-	# Überprüfe, ob alle erforderlichen Variablen gesetzt sind
-	if [ -z "$OUTDIR" ] || [ -z "$THREADS" ]; then
-		echo "$(datetime.error) Erforderliche Variablen fehlen."
-		exit 1
-	fi
-
-	# Ausgabe des Startzeitpunkts in die Protokolldatei
-	echo "$(datetime.task) Starte Binning mit Metawrap" >> "$BINNING_LOG"
-	echo "$(datetime.task) Starte Binning mit Metawrap" >> "$G_LOG_FILE"
-
-	# Metawrap Binning
-	mkdir -p "$OUTDIR/03-Bins"
-	mkdir -p "$OUTDIR/03-Bins/Binning"
-
-	# Binning mit Metawrap
-	metawrap binning -t "$THREADS" -a "$OUTDIR/02-De_Novo_Assembly/Flye/assembly.fasta" -o "$OUTDIR/03-Bins/Binning" --metabat2 --maxbin2 --single-end "$OUTDIR/00-InputData/"*fastq
-
-	# Ausgabe, dass der Vorgang abgeschlossen ist und Protokoll in die Datei schreiben
-	echo "$(datetime.done) Binning mit Metawrap abgeschlossen. Ausgabedaten in $OUTDIR/03-Bins/Binning" | tee -a "$BINNING_LOG"
-	echo "$(datetime.done) Binning mit Metawrap abgeschlossen" >> "$G_LOG_FILE"
-	echo "$(datetime.done) ------------------------------------------------------------------------------------------------------------------------" >> "$G_LOG_FILE"
-	
-	### Binning-Verfeinerung mit Metawrap
-
-	# Definiere den Pfad zur Protokolldatei
-	BIN_REFINEMENT_LOG="$OUTDIR/03-Bins/bin_refinement.log"
-
-	# Erstelle das Verzeichnis, falls es nicht vorhanden ist
-	mkdir -p "$(dirname "$BIN_REFINEMENT_LOG")"
-
-	# Ausgabe des Startzeitpunkts in die Protokolldatei
-	echo "$(datetime.task) Starte Binning-Verfeinerung mit Metawrap" >> "$BIN_REFINEMENT_LOG"
-	echo "$(datetime.task) Starte Binning-Verfeinerung mit Metawrap" >> "$G_LOG_FILE"
-
-	# Metawrap Binning-Verfeinerung
-	mkdir -p "$OUTDIR/03-Bins/Bin_Refinement"
-
-	# Binning-Verfeinerung mit Metawrap
-	metawrap bin_refinement -t "$THREADS" -A "$OUTDIR/03-Bins/Binning/metabat2_bins" -B "$OUTDIR/03-Bins/Binning/maxbin2_bins" -o "$OUTDIR/03-Bins/Bin_Refinement" --skip-checkm
-
-	# Ausgabe, dass der Vorgang abgeschlossen ist und Protokoll in die Datei schreiben
-	echo "$(datetime.done) Binning-Verfeinerung mit Metawrap abgeschlossen. Ausgabedaten in $OUTDIR/03-Bins/Bin_Refinement" | tee -a "$BIN_REFINEMENT_LOG"
-	echo "$(datetime.done) Binning-Verfeinerung mit Metawrap abgeschlossen" >> "$G_LOG_FILE"
-	echo "$(datetime.done) ------------------------------------------------------------------------------------------------------------------------" >> "$G_LOG_FILE"
-
-	# Ausgabe, dass der Vorgang abgeschlossen ist und Protokoll in die Datei schreiben für die Split-Funktion
-	echo "$(datetime.done) Split-Funktion abgeschlossen." | tee -a "$SPLIT_LOG"
-	echo "$(datetime.done) Split-Funktion abgeschlossen" >> "$G_LOG_FILE"
-	echo "$(datetime.done) ------------------------------------------------------------------------------" >> "$G_LOG_FILE"
-	echo "$(datetime.done)  ___ ___    ___ ______   ____  __    __  ____    ____  ____       ___     ___   ____     ___ "
-	echo "$(datetime.done) |   |   |  /  _]      | /    ||  |__|  ||    \  /    ||    \     |   \   /   \ |    \   /  _]"
-	echo "$(datetime.done) | _   _ | /  [_|      ||  o  ||  |  |  ||  D  )|  o  ||  o  )    |    \ |     ||  _  | /  [_ "
-	echo "$(datetime.done) |  \_/  ||    _]_|  |_||     ||  |  |  ||    / |     ||   _/     |  D  ||  O  ||  |  ||    _]"
-	echo "$(datetime.done) |   |   ||   [_  |  |  |  _  ||        ||    \ |  _  ||  |       |     ||     ||  |  ||   [_ "
-	echo "$(datetime.done) |   |   ||     | |  |  |  |  | \      / |  .  \|  |  ||  |       |     ||     ||  |  ||     |"
-	echo "$(datetime.done) |___|___||_____| |__|  |__|__|  \_/\_/  |__|\_||__|__||__|       |_____| \___/ |__|__||_____|"
-fi
 #-------------------------------------------------------------------------------------------------------------------------------------
 
 # Wenn der Ordner $OUTDIR/05-Results bereits existiert, dann überspringe
@@ -322,99 +205,11 @@ if [ -d "$OUTDIR/05-Results" ]; then
 	echo "$(datetime.skip) Klassifizierung mit GTDB-Tk wird uebersprungen" >> "$G_LOG_FILE"
 	echo "$(datetime.skip) Klassifizierung mit GTDB-Tk wird uebersprungen" 
 
-		# Definiere den Pfad zur Protokolldatei
-		CLASSIFY_LOG="$OUTDIR/04-Classify/classify_with_gtdbtk.log"
 
 		# Erstelle das Verzeichnis, falls es nicht vorhanden ist
 		mkdir -p "$(dirname "$CLASSIFY_LOG")"
 
 else
-	### Klassifizierung mit GTDB-Tk
-	
-	classify_gtdbtk(){
-
-		# Ausgabe des Startzeitpunkts in die Protokolldatei
-		echo "$(datetime.task) Starte Klassifizierung mit GTDB-Tk" >> "$CLASSIFY_LOG"
-		echo "$(datetime.task) Starte Klassifizierung mit GTDB-Tk" >> "$G_LOG_FILE"
-
-		# classify mit GTDB-Tk
-		mkdir -p "$OUTDIR/04-Classify"
-		mkdir -p "$OUTDIR/04-Classify/GTDBTK"
-		gtdbtk classify_wf --genome_dir "$OUTDIR/03-Bins/Bin_Refinement/work_files/binsM" --out_dir "$OUTDIR/04-Classify/GTDBTK" --cpus "$THREADS" --extension fa --skip_ani_screen --min_perc_aa 0.5
-
-		# Ausgabe, dass der Vorgang abgeschlossen ist und Protokoll in die Datei schreiben
-		echo "$(datetime.done) Klassifizierung mit GTDB-Tk abgeschlossen. Ausgabedaten in $OUTDIR/04-Classify/GTDBTK" >> "$CLASSIFY_LOG"
-		echo "$(datetime.done) Klassifizierung mit GTDB-Tk abgeschlossen" >> "$G_LOG_FILE"
-		echo "$(datetime.done) ------------------------------------------------------------------------------------------------------------------------" >> "$G_LOG_FILE"
-}
-
-# Aufruf der Funktion zum Klassifizieren
-classify_gtdbtk
-
-fi
-
-
-### Aufbereitung der Ergebnisse
-# Prüfe, ob der Ausgabeordner bereits existiert
-if [ -d "$OUTDIR/05-Classify" ]; then
-	# Wenn der Ausgabeordner existiert, entferne ihn und vermerke es im Terminal und Log
-	echo "$(datetime.warning) Entferne vorhandenen Ausgabeordner $OUTDIR/05-Results" >> "$G_LOG_FILE"
-	echo "$(datetime.warning) Entferne vorhandenen Ausgabeordner $OUTDIR/05-Results"
-	rm -r "$OUTDIR/05-Results"
-fi
-
-# Funktion zur Verarbeitung des GTDB-Tk-Ausgangs aufrufen
-process_gtdbtk_output() {
-	# Ausgabe des Startzeitpunkts in die Protokolldatei
-	echo "$(datetime.task) Starte Aufbereitung der GTDB-Tk Ergebnisse" >> "$CLASSIFY_LOG"
-	echo "$(datetime.task) Starte Aufbereitung der GTDB-Tk Ergebnisse" >> "$G_LOG_FILE"
-
-	gtdbtk_output_dir="$1"
-
-	# Ein leeres Array erstellen, um die Anzahl jedes Stammes zu zählen
-	declare -A stamm_count
-
-	# Pfad zur TSV-Datei unter Verwendung von gtdbtk_output_dir
-	tsv_paths=("$gtdbtk_output_dir"/gtdbtk*.summary.tsv)
-
-	# Erstelle das Verzeichnis für die Ergebnisdatei, falls es nicht vorhanden ist
-	results_dir="$OUTDIR/05-Results"
-	mkdir -p "$results_dir"
-
-	# CSV-Datei erstellen
-	results_csv_path="$results_dir/ergebnisse.csv"
-	# Header in die CSV-Datei schreiben
-	echo "Stamm,Count" > "$results_csv_path"
-
-	# Für jede TSV-Datei im Verzeichnis iterieren
-	for tsv_path in "${tsv_paths[@]}"; do
-		# Überprüfe, ob die TSV-Datei vorhanden ist
-		if [ -f "$tsv_path" ]; then
-			# CSV-Datei bearbeiten
-			awk 'NR>1 { print $2 }' FS='\t' OFS=',' "$tsv_path" | sort | uniq -c | awk '{ print $2","$1 }' >>"$results_csv_path"
-
-			echo "Verarbeitung von $tsv_path abgeschlossen."
-		else
-			echo "Die Datei $tsv_path existiert nicht."
-		fi
-	done
-
-	# Ausgabe, dass der Vorgang abgeschlossen ist und Protokoll in die Datei schreiben
-	echo "$(datetime.done) Aufbereitung GTDB-Tk abgeschlossen. Ausgabedaten in $OUTDIR/05-Results/ergebnisse.csv" | tee -a "$CLASSIFY_LOG"
-	echo "$(datetime.done) Aufbereitung GTDB-Tk abgeschlossen" >> "$G_LOG_FILE"
-	echo "$(datetime.done) ------------------------------------------------------------------------------------------------------------------------" >> "$G_LOG_FILE"
-}
-
-# Aufruf der Funktion zur Verarbeitung des GTDB-Tk-Ausgangs
-process_gtdbtk_output "$OUTDIR/04-Classify/GTDBTK"
-#-------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-#-------------------------------------------------------------------------------------------------------------------------------------
 
 # Definiere den Pfad zur Protokolldatei für BLASTn
 BLASTN_LOG="$OUTDIR/05-Results/blastn.log"
@@ -466,16 +261,6 @@ run_blastn() {
 run_blastn "$OUTDIR/01-CleanedReads/align_to_ref_unaligned.fa"
 
 #-------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
 
 
 ### Klassifizierung mit BLASTn
@@ -607,3 +392,12 @@ sleep 20
 
 # Aufruf der Funktion Shiny GUI
 shiny_gui
+
+
+
+fi
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
